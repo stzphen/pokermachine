70c6bcc1e3fd9732741de2eb6f7c7c71bddc1119
Stephen Zhong <stephenyzhong@gmail.com>
2022-06-23 00:04:48 -0700

    started game class: find players/stacks/holeCards
    Co-authored-by: williambtong <williambtong@users.noreply.github.com>
    Co-authored-by: Jeffrey-Mi <Jeffrey-Mi@users.noreply.github.com>

--- poker.py
+++ poker.py
@@ -1,5 +1,6 @@
 import random
 import itertools
+import regex
 from pokerDict import pokerDict
 
 CARDS = {2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9", 10:"T", 11:"J", 12:"Q", 13:"K", 14:"A"}
@@ -7,6 +8,7 @@ RANKIDS = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "T":10, "J":1
 RANKS = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
 SUITIDS = {"c":0, "d":1, "h":2, "s":3}
 SUITS = ("c", "d", "h", "s")
+SUITSYMBOLS = {"♣": "c", "♦": "d", "♥": "h", "♠": "s"}
 POSITIONS = ("SB", "BB", "UTG", "UTG1", "LJ", "HJ", "CO", "D")
 suitedID = 1 << 20
 
@@ -83,16 +85,27 @@ class Deck(object):
     
 
 class Player(object):
-    def __init__(self, name, stack):
+    def __init__(self, name, stack, position, hero, holeCards = None):
         self.name = name
+        self.position = position
         self.stack = stack
-        self.holeCards = []
-        pass
+        self.hero = hero
+        if hero:
+            self.holeCards = holeCards
+    
+    def debugPrintInfo(self):
+        print(f"player: {self.name}\n")
+        print(f"stack: {self.stack}\n")
+        print(f"position: {self.position}\n")
+        if self.hero:
+            print(f"holeCards: {self.holeCards}\n")
 
 
 class Table(object):
     def __init__(self):
         self.board = []
+        self.pot = 0
+        self.deck = Deck()
 
     def dealFlop(self, deck):
         for i in range(3):
@@ -106,21 +119,113 @@ class Table(object):
 
 
 class Hand(object): #should change hand so it takes in a list, generates best hand
-    def __init__(self, player: Player, table: Table):
-        self.choices = player.holeCards + table.board
+    def __init__(self, holecards: list, table: Table):
+        self.choices = holecards + table.board
+        self.bestHand = self.generateBestHand()
+        self.handID = self.generateHandID(self.bestHand)
 
-    #only generate with 5 cards
+    def generateHandID(self, hand : list) -> int:
+        # generate hand ID
+        handID = 0
+        for i in range(5):
+            handID |= (hand[i].rank << (4 * (4 - i)))
+        if hand[0].suit == hand[1].suit and hand[1].suit == hand[2].suit and hand[2].suit == hand[3].suit and hand[3].suit == hand[4].suit:
+            handID |= suitedID
+        # print(readableHandID(handID))
+        return handID
+
+    def generateBestHand(self) -> int:
+        cards = self.choices
+        allCombos = list(itertools.combinations(cards, 5))
+        minValue = 1000000000
+        for hand in allCombos:
+            generatedValue = self.generateHandID(hand)
+            if pokerDict[generatedValue] < minValue:
+                minValue = pokerDict[generatedValue]
+        return minValue
 
     def compareHands(self, other):
         pass
 
 
 class Game(object):
-    def __init__(self, sb, bb):
-        self.players = []
-        self.smallBlind = sb
-        self.bigBlind = bb
-        pass
+    def __init__(self, filename, hero):
+        self.parseText(filename, hero)
+
+    def parseText(self, filename, hero):
+        #read in game log
+        with open(filename) as f:
+            log = f.readlines()
+            log.reverse()
+        #get rid of unnecessary lines
+        i = 0
+        while i < len(log):
+            if len(log[i]) <= 6:
+                log.remove(log[i]) 
+            else:
+                log[i] = log[i].strip()
+                i += 1
+        print(log)
+        # find dealer
+        dealExp = regex.compile("dealer: \S+")
+        tmpDeal = dealExp.findall(log[0])[0]
+        dealer = tmpDeal[8:-1]
+        print(dealer)
+
+        # find names and stacks, rotate so sb -> dealer
+        playerExp = regex.compile("#\d+ \S+")
+        stackExp = regex.compile("\((\d+(\.\d+)?)\)")
+
+        tmpPlay = playerExp.findall(log[1])
+        print(tmpPlay)
+        tmpStack = stackExp.findall(log[1])
+        print(tmpStack)
+
+        names = []
+        stacks = []
+
+        for i in range(len(tmpPlay)):
+            names.append(tmpPlay[i].split(" ")[1])
+            stacks.append(tmpStack[i][0])
+        assert(len(stacks) == len(tmpPlay) and len(names) == len(tmpPlay))
+
+        d = names.index(dealer)
+        names = names[(d + 1) % len(names):] + names[:(d + 1) % len(names)]
+        stacks = stacks[(d + 1) % len(stacks):] + stacks[:(d + 1) % len(stacks)]
+        print(names)
+        print(stacks)
+
+        # read hole cards
+        if log[2][:13] == "Your hand is ":
+            tmpCards = log[2][13:].split(",")
+            print(tmpCards)
+            holeCards = []
+            for card in tmpCards:
+                card = card.strip()
+                rank = card[0]
+                suit = SUITSYMBOLS[card[1]]
+                holeCards.append(Card(rank+suit))
+            log.remove(log[2])
+        
+        print(log)
+
+        #actually make players
+        players = []
+        for j in range(len(names)):
+            if hero == names[j]:
+                players.append(Player(names[j], stacks[j], POSITIONS[j], True, holeCards))
+            else:
+                players.append(Player(names[j], stacks[j], POSITIONS[j], False))
+        
+        for play in players:
+            play.debugPrintInfo()
+
+        # start reading game
+
+        # initizlie table
+            # board
+            # pot
+        pass #read hand, set info
 
     def startHand(self):
         self.deck = Deck().shuffle()
@@ -138,24 +243,4 @@ class Game(object):
     def restartHand(self):
         pass
 
-def generateHandID(hand: list) -> int:
-    # generate hand ID
-    handID = 0
-    for i in range(5):
-        handID |= (hand[i].rank << (4 * (4 - i)))
-    if hand[0].suit == hand[1].suit and hand[1].suit == hand[2].suit and hand[2].suit == hand[3].suit and hand[3].suit == hand[4].suit:
-        handID |= suitedID
-    # print(readableHandID(handID))
-    return handID
-
-def generateBestHand(cards: list) -> int:
-    allCombos = list(itertools.combinations(cards, 5))
-    minValue = 1000000000
-    for hand in allCombos:
-        generatedValue = generateHandID(list(hand))
-        if pokerDict[generatedValue] < minValue:
-            minValue = pokerDict[generatedValue]
-    return minValue
-
-
-
+Game("log.txt", "Jeff")
